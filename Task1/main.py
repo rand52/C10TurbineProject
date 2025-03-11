4b27d453afb47a934d203fd4ff4d805a2d3c5fc3
ylvandenakker <y.l.vandenakker@student.tudelft.nl>
2025-03-11 11:54:30 +0100

    changes to main

--- Task1/main.py
+++ Task1/main.py
@@ -4,3 +4,24 @@ Times, Wind1VelXs, Wind1VelYs, Wind1VelZs, GenPwrs = DataSplit("Task1/Data/noshe
 print(Times)
 print("Average power produced:", np.mean(GenPwrs))
 # print(DataSplit("Task1/Data/noshear TIC_veer0.2/Extracted_Uref3.csv"))
+
+wind = []
+power = []
+
+wind.np.mean(Wind1VelXs)
+power.np.mean(GenPwrs)
+
+
+
+import matplotlib.pyplot as plt
+
+# Create the plot
+plt.plot(Times, GenPwrs, marker='o', linestyle='-')
+
+# Labels and title
+plt.xlabel('Time')
+plt.ylabel('Power')
+plt.title('Simple Plot')
+
+# Show the plot
+plt.show()
