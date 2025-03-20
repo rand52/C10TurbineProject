2cd26e9908c9ce7d63bc28ef9c517bfdb7353d86
rand52 <124181900+rand52@users.noreply.github.com>
2025-03-20 15:44:57 +0100

    changes to the file reader

--- Task2/data_reader.py
+++ Task2/data_reader.py
@@ -1,8 +1,9 @@
 import numpy as np
-import math as mt
+import os
 
 # File path
-file_path = "Data/U_withAGW"
+current_file_path = os.path.abspath(__file__)
+file_path = os.path.join(os.path.dirname(current_file_path), 'Data', 'U_withAGW')
 
 # time offset of start of data gathering since simulation start
 time_start_point = 21600
--- Task2/meshpoints_plotter.py
+++ Task2/meshpoints_plotter.py
@@ -1,13 +1,10 @@
-import data_reader_Dillon as data
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 # Importing data from data_reader_Dillon
-from data_reader_Dillon import get_probe_locations, get_velocity_data, Vx_index, Vy_index, Vz_index
+from data_reader import get_probe_locations, get_velocity_data, Vx_index, Vy_index, Vz_index
 
-velocity_data, time_steps = get_velocity_data(time_between_datapts=1)
+velocity_data, time_steps = get_velocity_data(time_between_datapts=50)
 
 # Debugging step: Print shapes
 print(f"time_steps shape: {time_steps.shape}")
